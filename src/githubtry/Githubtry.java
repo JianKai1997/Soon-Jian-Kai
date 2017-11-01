88659a693032f9155c39633ccc1d1d70b1b0eb04
B1 <B1@B148>
2017-11-01 13:17:49 +0800

    My first commit

--- src/githubtry/Githubtry.java
+++ src/githubtry/Githubtry.java
@@ -5,6 +5,8 @@ public class Githubtry {
     public static void main(String[] args) {
         
         System.out.println("Hello , its me");
+        System.out.println("I eat vege");
+        System.out.println("Do you want to try some vegan bacon ?");
         
     }
     
